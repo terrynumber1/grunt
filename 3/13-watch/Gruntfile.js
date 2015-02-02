f059ba25f52197d2a27b47172fb9923bf99c588f
Andy <Andy@mail.com>
2015-02-02 06:42:07 -0500

    re-indented Gruntfile.js in 13-watch

--- 3/13-watch/Gruntfile.js
+++ 3/13-watch/Gruntfile.js
@@ -4,20 +4,20 @@ grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-watch');
 // Project configuration.
 grunt.initConfig({
-srcFiles: ["src/a.js", "src/b.js", "src/c.js"],
-concat: {
-target1: {
-files: {
-"build/abc.js": "<%= srcFiles %>"
-}
-}
-},
-watch: {
-target1: {
-files: "<%= srcFiles %>",
-tasks: ["concat"]
-}
-}
+	srcFiles: ["src/a.js", "src/b.js", "src/c.js"],
+	concat: {
+		target1: {
+			files: {
+				"build/abc.js": "<%= srcFiles %>"
+			}
+		}
+	},
+	watch: {
+		target1: {
+			files: "<%= srcFiles %>",
+			tasks: ["concat"]
+		}
+	}
 });
 // Define the default task
 grunt.registerTask('default', ['concat', 'watch']);
